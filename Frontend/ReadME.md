cd621ce9ba539dababbd214dfa996be1eb1ac4d9
Mazen Olama <mazen.olama00@gmail.com>
2023-08-16 19:39:56 +0300

    map finished

---
+++ Frontend/ReadME.md
@@ -0,0 +1,21 @@
+# centuty21-cyprus
+
+Contact Us Page using Restful API that allows API consumers to send email using SMTP and save it in NoSQL database, behind the scene it uses Nodejs, Express and MongoDB.
+
+## Dependecies
+[NodeJs](https://nodejs.org/en/) - A JavaScript runtime environment\
+[Express](https://expressjs.com/) - An Express framework\
+[MongoDB](https://www.mongodb.com/) - A NoSQL database
+
+## Setup and run
+to setup this project, run the following commands on your terminal
+```bash
+1. to clone the project `git clone https://github.com/mazenolama/centuty21-cyprus.git`
+2. navigate to the project directory `cd centuty21-cyprus/Backend`
+3. install neccesary packages, modules `npm install`
+4. to run the project `npm start`
+5. using POSTMAN you can use this default url of `http://localhost:3000`
+```
+
+## Author
+[Mazen Olama]
--- Frontend/assets/js/map.js
+++ Frontend/assets/js/map.js
@@ -2,30 +2,67 @@
 let map;
 
 function initMap() {
-  map = new google.maps.Map(document.querySelector("#map"), {
-    mapId: "6d7fac2f87f28a6d",
-    center: { lat: 34.7061323, lng: 33.1166691 },
-    zoom: 12,
-    disableDefaultUI: true,
-  });
-  initZoomControl(map);
+    map = new google.maps.Map(document.querySelector("#map"), {
+        mapId: "6d7fac2f87f28a6d",
+        center: { lat: 34.6665442278154, lng: 33.00678164597041 },
+        zoom: 14,
+        disableDefaultUI: true,
+        zoomControl: true,
+        gestureHandling: "greedy",
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.TOP_RIGHT,
+        },
+    });
+
+    const branches = [
+        {
+            position: { lat: 34.67023408576484, lng: 33.01611878371115 },
+            icon: "assets/img/active-geotag-icon.png",
+            tabClass: "branch-tab-0",
+            boxClass: "branch-box-0",
+        },
+        {
+            position: { lat: 34.663183938045364, lng: 33.01417444321938 },
+            icon: "assets/img/geotag-icon.png",
+            tabClass: "branch-tab-1",
+            boxClass: "branch-box-1",
+        },
+        {
+            position: { lat: 34.6583832878914, lng: 32.99997249015476 },
+            icon: "assets/img/geotag-icon.png",
+            tabClass: "branch-tab-2",
+            boxClass: "branch-box-2",
+        },
+    ];
+
+    const markers = branches.map((branch) => {
+        const marker = new google.maps.Marker({
+            position: branch.position,
+            map,
+            animation: google.maps.Animation.DROP,
+            icon: {
+                url: branch.icon,
+                scaledSize: new google.maps.Size(36, 48),
+            },
+        });
+
+        marker.addListener("click", function () {
+            $(".branch-tab").removeClass("active-tab");
+            $(".branch-box").hide();
+            $(`.${branch.tabClass}`).addClass("active-tab");
+            $(`.${branch.boxClass}`).show();
+
+            // Reset icons of all markers
+            markers.forEach((m) => {
+                m.setIcon({
+                    url: m === marker ? "assets/img/active-geotag-icon.png" : "assets/img/geotag-icon.png",
+                    scaledSize: new google.maps.Size(36, 48),
+                });
+            });
+        });
+
+        return marker;
+    });
 }
-
-function initZoomControl(map) {
-  document.querySelector(".zoom-control-in").onclick = function () {
-    map.setZoom(map.getZoom() + 1);
-  };
-
-  document.querySelector(".zoom-control-out").onclick = function () {
-    map.setZoom(map.getZoom() - 1);
-  };
-
-  map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(
-    document.querySelector(".zoom-control"),
-  );
-}
-
-
-
 
 window.initMap = initMap;
